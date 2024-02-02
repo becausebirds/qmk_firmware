610e60c5d6b58dbb92cbff9909339e48d5a92b1e
Jeff Mann <110354170+becausebirds@users.noreply.github.com>
2024-02-01 23:08:18 -0600

    OS Detection enabled

--- keyboards/planck/rev7/keymaps/becausebirds/keymap.c
+++ keyboards/planck/rev7/keymaps/becausebirds/keymap.c
@@ -15,15 +15,17 @@
  */
 
 #include QMK_KEYBOARD_H
+#include "os_detection.h"
 
 enum planck_layers { _QWERTY, _LOWER, _RAISE, _NUMBERS };
-
+enum custom_keycodes { KC_CMD_CTRL = SAFE_RANGE };
 enum planck_keycodes { QWERTY = SAFE_RANGE };
 
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
 #define NUMBERS TO(_NUMBERS)
 #define KC_CAD	LALT(LCTL(KC_DEL))
+
 
 /* clang-format off */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -36,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 |------+------+------+------+------+------+------+------+------+------+------+------|
 | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
 |------+------+------+------+------+------+------+------+------+------+------+------|
-| Ctrl | LGUI | Alt  |TO_Num| Lower|    Space    |Raise | Left | Down |  Up  |Right |
+|CmdCtl| LGUI | Alt  |TO_Num| Lower|    Space    |Raise | Left | Down |  Up  |Right |
 `-----------------------------------------------------------------------------------'
 
  */
@@ -44,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT),
-    KC_LCTL, KC_LGUI, KC_LALT, KC_NO,  LOWER,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_CMD_CTRL, KC_LGUI, KC_LALT, KC_NO,  LOWER,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Lower
@@ -61,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_planck_1x2uC(
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   LCMD(KC_T),   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  LCTL(KC_C),  LCTL(KC_V),  KC_NO,      _______,      _______,   _______,   _______,
+    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  LCTL(KC_C),  LCTL(KC_V),      _______,      _______,      _______,   _______,   _______,
     _______, _______, _______, _______, _______, KC_SPC, TO(NUMBERS), _______,    KC_MUTE,    KC_VOLD,    KC_VOLU
 ),
 
@@ -101,3 +103,22 @@ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_1, KC_2, KC_3, KC_EQL
 KC_TRNS, KC_TRNS, KC_TRNS, TO(0), TO(0), KC_SPC, TO(0), KC_0, KC_0, KC_DOT, KC_ENT
 ),
 };
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case KC_CMD_CTRL:
+            if (record->event.pressed) {
+                if (detected_host_os() == OS_MACOS) {
+                    register_code(KC_LGUI);  // Command key on Mac
+                } else if (detected_host_os() == OS_WINDOWS) {
+                    register_code(KC_LCTL);  // Control key on Windows
+                }
+            } else {
+                unregister_code(KC_LGUI);
+                unregister_code(KC_LCTL);
+            }
+            return false;
+        default:
+            return true;
+    }
+}
