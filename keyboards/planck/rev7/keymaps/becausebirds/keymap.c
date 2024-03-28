065f41e723265c1753d3846d887a117a2d319c95
Jeff Mann <110354170+becausebirds@users.noreply.github.com>
2024-03-28 09:03:11 -0500

    period and upper and lower layers

--- keyboards/planck/rev7/keymaps/becausebirds/keymap.c
+++ keyboards/planck/rev7/keymaps/becausebirds/keymap.c
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_planck_1x2uC(
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
     KC_DEL,  KC_F1,   KC_F2,   ALT_TAB,   KC_FIND,   KC_F5,   LCMD(KC_T),   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
-    KC_NO,   KC_F7,   KC_UNDO,   KC_ALL,   KC_CUT,  KC_COPY,  KC_PASTE,      KC_NO,      KC_NO,      KC_NO,   KC_NO,   KC_NO,
+    KC_NO,   KC_F7,   KC_UNDO,   KC_ALL,   KC_CUT,  KC_COPY,  KC_PASTE,      KC_NO,      KC_NO,      KC_DOT,   KC_NO,   KC_NO,
     KC_1PASS,   KC_NO,   KC_NO,   KC_NEW_TAB,   KC_NO,   KC_SPLT,   TO(NUMBERS),   KC_NO,      KC_MUTE,    KC_VOLD,    KC_VOLU
 ),
 
@@ -89,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT_planck_1x2uC(
     KC_MINS,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PLUS,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-    KC_NO,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+    KC_NO,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NO,   KC_NO,   KC_DOT,   KC_NO,   KC_NO,
     RGB_TOG, RGB_VAD, RGB_VAI, RGB_MOD, TO(NUMBERS), KC_SPC, KC_NO,   KC_SCRSHT,   KC_MUTE, KC_VOLD, KC_VOLU
 ),
 
